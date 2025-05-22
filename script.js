8bce07f8322c58d3ebbe6ca99c54601868eccd58
Craigz <6alactico>
2025-05-21 22:18:49 -0500

    commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const form = document.getElementById('form');
+const newsletter = document.getElementById('newsletter-container');
+const success = document.getElementById('success-container');
 
 // Retrieve data from form
 const handleSubmit = (e) => {
-    e.preventDefault(e);
+    e.preventDefault();
 
     const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
+    const data = Object.fromEntries(formData.entries());
 
-    console.log(data);
-}
+    newsletter.classList.add('hidden');
+    success.classList.remove('hidden');
+    
+    console.log('Form submitted');
+};
 
 form.addEventListener('submit', handleSubmit);
