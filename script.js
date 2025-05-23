b9b88fa6f0e825b277cfd0f9083793ee6fd3d5aa
Craigz <6alactico>
2025-05-22 20:39:38 -0500

    commit updates

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ const form = document.getElementById('form');
 const newsletter = document.getElementById('newsletter-container');
 const success = document.getElementById('success-container');
 const dismissButton = document.querySelector('.dismiss');
+const emailInput = document.getElementById('email-address');
+const errorMessage = document.getElementById('error-message');
+
 
 // Retrieve data from form
 form.addEventListener ('submit', (e) => {
     e.preventDefault(); // Prevent default
 
-    let formData = new FormData(e.target); // Form data object
-    let data = Object.fromEntries(formData.entries());
+    const emailValue = emailInput.value.trim();
+
+    // Reset state error
+    emailInput.classList.remove('invalid');
+    errorMessage.textContent = '';
+
+    // Custom email validation
+    const isValidEmail = /\S+@\S+\.\S+/.test(emailValue);
+
+    if (!isValidEmail) {
+        errorMessage.textContent = 'Valid email required';
+        emailInput.classList.add('invalid');
+        return;
+    }
+
+    // If valid, proceed
+    const formData = new FormData(e.target); // Form data object
+    const data = Object.fromEntries(formData.entries());
 
     newsletter.classList.add('hidden');
     success.classList.remove('hidden');
-    
     console.log(data);
 });
 
@@ -20,4 +38,8 @@ dismissButton.addEventListener('click', () => {
     success.classList.add('hidden'); // Hide element
     newsletter.classList.remove('hidden'); // Show element
     form.reset();
+
+    // Reset errors
+    errorMessage.textContent = '';
+    emailInput.classList.remove('invalid');
 });
